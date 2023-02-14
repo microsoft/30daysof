7c2afca945ad931d8296eb78969516dc7c5ac8ed
Nitya Narasimhan <n.narasimhan@gmail.com>
2023-02-13 19:42:54 -0500

    fix #176 - lock down themes to preferred light/dark to ensure contrast remains valid

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -81,7 +81,7 @@ const config = {
       colorMode: {
         defaultMode: "light",
         disableSwitch: false,
-        respectPrefersColorScheme: true,
+        respectPrefersColorScheme: false,
       },
 
       navbar: {
